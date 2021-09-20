f981a824b5e3fe4e3ffd4cda2182faed33185b9a
Rafal Lesnicki <lesnickirafal@gmail.com>
2021-09-20 18:51:15 +0200

    homework

---
+++ src/test/java/trello/trellozadaniedomowe/HomeworkOrganizationTest.java
@@ -0,0 +1,62 @@
+package trello.trellozadaniedomowe;
+
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import trello.Organization;
+
+import java.util.stream.Stream;
+
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HomeworkOrganizationTest {
+
+
+    protected static final String KEY = "3dce6f67a1fc2ee98c14c98e1e310569";
+    protected static final String TOKEN = "773524badb9b285294a43350a4b640169c0f47cc729248cfd9251887527fd33e";
+
+    private static Stream<Arguments> createOrganizationData() {
+        return Stream.of(
+
+                Arguments.of("Diplay name", "Testowe", "imie testowe", "htt://www.testowe.com"),
+                Arguments.of("Diplay name", "Testowe", "IMIE", "https://www.testowe.com"),
+                Arguments.of("Diplay name", "Testowe", "im", "https://www.testowe.com"),
+                Arguments.of("Diplay name", "Testowe", ":::", "https://www.testowe.com"),
+                Arguments.of("Diplay name", "Testowe", "!!!", "https://www.testowe.com"));
+    }
+
+    @DisplayName("Create organization with valida data")
+    @ParameterizedTest(name = "Display name: {0}, desc: {1}, name: {2}, website: {3}")
+    @MethodSource("createOrganizationData")
+    public void createOrganizationWithInvalidData(String displayName, String desc, String name, String website) {
+        Organization organization = new Organization();
+
+        organization.setDisplayName(displayName);
+        organization.setDesc(desc);
+        organization.setName(name);
+        organization.setWebsite(website);
+
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .queryParam("key", KEY)
+                .queryParam("token", TOKEN)
+                .queryParam("displayName", organization.getDisplayName())
+                .queryParam("desc", organization.getDesc())
+                .queryParam("name", organization.getName())
+                .queryParam("website", organization.getWebsite())
+                .when()
+                .post("https://api.trello.com/1/organizations")
+                .then()
+                .statusCode(400)
+                .extract()
+                .response();
+
+        JsonPath json = response.jsonPath();
+        assertThat(json.getString("displayName")).isEqualTo(organization.getDisplayName());
+    }
+}
